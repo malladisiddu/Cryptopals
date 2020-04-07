15ebc71df0b91c9fe4102a8fec567e9f346fd89e
Malladi Siddartha <46401317+malladisiddu@users.noreply.github.com>
2020-04-07 22:07:18 +0530

    Delete singlebyte_xor.py

--- singlebyte_xor.py
+++
@@ -1,9 +0,0 @@
-import codecs
-from string import ascii_uppercase as chars
-a = codecs.decode(input(),'hex').decode('utf-8')
-b = ""
-
-for i in chars:
-	for j in range(len(a)):
-		b += chr(ord(a[j]) ^ ord(i))
-print(b)
